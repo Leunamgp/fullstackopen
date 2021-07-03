999183654f0c380b129c64453709c0c7ba0e0644
Leunamgp <leunamgp@gmail.com>
2021-07-02 22:51:38 -0400

    1.3-1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,48 +5,55 @@ const Header = (props) => {
   return (<h1>{props.course}</h1>)
 }
 
+
+const Total = (props) => {
+  let sum = 0
+  props.total.map(element => sum += element.exercises);
+  return (<p>{sum}</p>)
+}
+
 const Part = (props)=>{
   return (
     <p>{props.part} {props.exercises}</p>
   )
 }
+
  const Content = (props) => {
    const content = props.content;
 
    const data = content.map(element => {
     return (
-    <Part part={element.part} exercises={element.exercises} />
+    <Part part={element.name} exercises={element.exercises} />
     )
   })  
-
   return data
  }
 
-const Total = (props) => {
-  return (<p>Number of exercises {props.total}</p>)
-}
-  const App = () => {
-
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+ const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-     <Header course={course}/>
-     <Content content={[
-                          {part:part1, exercises:exercises1}, 
-                          {part:part2, exercises:exercises2}, 
-                          {part:part3, exercises:exercises3}
-                      ]}/>
-
-     <Total total={exercises1 + exercises2 + exercises3}/>
-    </div>
-  )
+     <Header course={course.name}/>
+     <Content content={course.parts}/>
+     <Total total={course.parts}/>
+    </div>)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
